cad832ca96137e9b1728b03e34d066f88db27ffb
Philipp <philipp.roytburg@rwth-aachen.de>
2019-08-09 16:48:25 +0200

    cleanup, documentation and readme

--- README.md
+++ README.md
@@ -1,7 +1,62 @@
-# Learning Locker Service
+Learning Locker Service
+===========================================
 This repository is part of the bachelor thesis 'A Multimodal Mentoring Cockpit for Tutor Support'.
 It is a las2peer Service, that enables the connection between MobSOS and Learning Locker 
+Other related repositories for the bachelor thesis can be found here: [Mentoring Cockpit](https://github.com/rwth-acis/Mentoring-Cockpit)
 
 
-# MobSOS to Learning Locker
-The MobSOS data processing services stores the MobSOS messages in a MySQL database. When an xAPI statement is stored in the remarks of a message a new statement, a service is invoked which passes the xAPI statement onto an LRS. See [property file](etc/i5.las2peer.services.learningLockerService.LearningLockerService.properties) to configure the address and authentication of the LRS.
+Learning Locker configuration
+--------------------------
+The [mobsos-data-processing service](https://github.com/rwth-acis/mobsos-data-processing/tree/bachelor-thesis-philipp-roytburg) stores the messages in a MySQL database. When an xAPI statement from a proxy service (e.g. [moodle-data-proxy](https://github.com/rwth-acis/moodle-data-proxy)) this service is invoked which passes the xAPI statement to a Learning Record Store (LRS).
+
+In Learning Locker an LRS is configured under Settings > Store > Add new.
+And under Settings > Client a corresponding client can be configured with the authentication.
+
+Service setup
+-------------
+To set up the service configure the [property file](etc/i5.las2peer.services.learningLockerService.LearningLockerService.properties) file with your LRS domain and the corresponding authentication.
+```INI
+lrsDomain = http://exampleDomain/data/xAPI/statements
+lrsAuth = exampleAuth
+```
+
+Build
+--------
+Execute the following command on your shell:
+
+```shell
+ant jar 
+```
+
+Start
+--------
+
+To start the learning-locker service, follow the [Starting-A-las2peer-Network tutorial](https://github.com/rwth-acis/las2peer-Template-Project/wiki/Starting-A-las2peer-Network) and bootstrap your service to a [mobsos-data-processing service](https://github.com/rwth-acis/mobsos-data-processing/tree/bachelor-thesis-philipp-roytburg).
+
+How to run using Docker
+-------------------
+
+First build the image:
+```bash
+docker build . -t learning-locker
+```
+
+Then you can run the image like this:
+
+```bash
+docker run -e LRS_DOMAIN=lrsDomain -e LRS_AUTH=lrsAuth -p 9011:port learning-locker
+```
+
+Replace *lrsDomain* with the domain of your LRS and *lrsAuth* with the corresponding authentication and *port* with a free port in your network.
+
+### Node Launcher Variables
+
+Set [las2peer node launcher options](https://github.com/rwth-acis/las2peer-Template-Project/wiki/L2pNodeLauncher-Commands#at-start-up) with these variables.
+The las2peer port is fixed at *9011*.
+
+| Variable | Default | Description |
+|----------|---------|-------------|
+| BOOTSTRAP | unset | Set the --bootstrap option to bootstrap with existing nodes. The container will wait for any bootstrap node to be available before continuing. |
+| SERVICE_PASSPHRASE | Passphrase | Set the second argument in *startService('<service@version>', '<SERVICE_PASSPHRASE>')*. |
+
+*Do not forget to persist you database data*
--- src/main/i5/las2peer/services/learningLockerService/LearningLockerService.java
+++ src/main/i5/las2peer/services/learningLockerService/LearningLockerService.java
@@ -16,86 +16,72 @@ import i5.las2peer.restMapper.annotations.ServicePath;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.Contact;
 import io.swagger.annotations.Info;
-import io.swagger.annotations.License;
 import io.swagger.annotations.SwaggerDefinition;
 
-// TODO Describe your own service
-/**
- * las2peer-Template-Service
- * 
- * This is a template for a very basic las2peer service that uses the las2peer WebConnector for RESTful access to it.
- * 
- * Note: If you plan on using Swagger you should adapt the information below in the SwaggerDefinition annotation to suit
- * your project. If you do not intend to provide a Swagger documentation of your service API, the entire Api and
- * SwaggerDefinition annotation should be removed.
- * 
- */
-// TODO Adjust the following configuration
 @Api
 @SwaggerDefinition(
-		info = @Info(
-				title = "las2peer Template Service",
-				version = "1.0.0",
-				description = "A las2peer Template Service for demonstration purposes.",
-				termsOfService = "http://your-terms-of-service-url.com",
-				contact = @Contact(
-						name = "John Doe",
-						url = "provider.com",
-						email = "john.doe@provider.com"),
-				license = @License(
-						name = "your software license name",
-						url = "http://your-software-license-url.com")))
+    info = @Info(
+        title = "Moodle Data Proxy Service",
+        version = "1.0",
+        description = "A proxy for requesting data from moodle",
+        contact = @Contact(
+            name = "Philipp Roytburg",
+            email = "philipp.roytburg@rwth-aachen.de")
+
+/**
+ * 
+ * This service is for sending xAPI statements that are monitored through MobSOS to the defined Learning Locker instance.
+ * 
+ */
 @ManualDeployment
 @ServicePath("/lrs")
-// TODO Your own service class
 public class LearningLockerService extends Service {
-	
-	private String lrsDomain;
-	private String lrsAuth;
-	
-	private static ArrayList<String> oldstatements = new ArrayList<String>();
-	
-	public LearningLockerService(){
-		setFieldValues();
-	}
-	
-	public void sendXAPIstatement(ArrayList<String> statements) {
-		for(String statement : statements) {
-			if(!oldstatements.contains(statement)) {
-				oldstatements.add(statement);
+    
+    private String lrsDomain; // Learning Locker Domain
+    private String lrsAuth; // Learning Locker Authentication
+    
+    private static ArrayList<String> oldstatements = new ArrayList<String>();
+    
+    public LearningLockerService(){
+        setFieldValues();
+    }
+    
+    /**
+     * A function that is called by the user to send processed moodle to a mobsos data processing instance. 
+     *
+     * @param statements is an ArrayList of xAPI statements that are sent to the defined Learning Locker instance.
+     * 
+     */
+    public void sendXAPIstatement(ArrayList<String> statements) {
+        for(String statement : statements) {
+            if(!oldstatements.contains(statement)) {
+                oldstatements.add(statement);
 
-				try {
-					URL url = new URL(lrsDomain);
-					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-					conn.setDoOutput(true);
-					conn.setDoInput(true);
-					conn.setRequestMethod("POST");
-					conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-					conn.setRequestProperty("X-Experience-API-Version","1.0.3");
-					conn.setRequestProperty("Authorization", lrsAuth);
-					conn.setRequestProperty("Cache-Control", "no-cache");
-					conn.setUseCaches(false);
-					
-					OutputStream os = conn.getOutputStream();
-					os.write(statement.getBytes("UTF-8"));
-					os.flush();
-					
-					Reader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-					
-					//maybe for frontend needed maybe not
-					for (int c; (c = reader.read()) >= 0;)
-						System.out.print((char)c);
-						
-					
-					conn.disconnect();
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+                try {
+                    URL url = new URL(lrsDomain);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                    conn.setRequestProperty("X-Experience-API-Version","1.0.3");
+                    conn.setRequestProperty("Authorization", lrsAuth);
+                    conn.setRequestProperty("Cache-Control", "no-cache");
+                    conn.setUseCaches(false);
+                    
+                    OutputStream os = conn.getOutputStream();
+                    os.write(statement.getBytes("UTF-8"));
+                    os.flush();
+                    
+                    conn.disconnect();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
-		}
-	}
+        }
+    }
 
 }
